a2ce463d32b0a7b56986f26f89e33ab626b5bf11
riyaj-ym <riyaj.ym@gmail.com>
2023-01-27 20:12:21 +0530

    added POM for Login page and added test method for verification of homepage title(for login test with valid credentials)

--- PageObjectModel/LoginPage.py
+++ PageObjectModel/LoginPage.py
@@ -2,4 +2,25 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPage:
-    pass
+    inputBox_UserEmail_ID = "Email"
+    inputBox_Password_ID = "Password"
+    button_Login_XPATH = '//button[normalize-space()="Log in"]'
+    link_Logout_LinkText = 'Logout'
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def setUserEmail(self, username):
+        self.driver.find_element(By.ID, self.inputBox_UserEmail_ID).clear()
+        self.driver.find_element(By.ID, self.inputBox_UserEmail_ID).send_keys(username)
+
+    def setUserPassword(self, password):
+        self.driver.find_element(By.ID, self.inputBox_Password_ID).clear()
+        self.driver.find_element(By.ID, self.inputBox_Password_ID).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element(By.XPATH, self.button_Login_XPATH).click()
+
+    def clickLogout(self):
+        self.driver.find_element(By.LINK_TEXT, self.link_Logout_LinkText).click()
+
--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -3,14 +3,15 @@ import os
 import pytest
 from utility.ReadProperties import ReadProperties
 from logs.LogGen import LogGen
+from PageObjectModel.LoginPage import LoginPage
 
 
-class TestLogin01:
+class TestLogin101:
     url = ReadProperties.getUrl()
     username = ReadProperties.getUserEmail()
     password = ReadProperties.getPassword()
     logger = LogGen.logGen()
-    logger.info("------------------ TestLogin01 ------------------ ")
+    logger.info("------------------ TestLogin101 ------------------ ")
 
     def test_url(self, setup):
         self.logger.info("------------------ Testing website url ------------------ ")
@@ -27,4 +28,24 @@ class TestLogin01:
         else:
             self.logger.info("------------------ website url testing got failed ------------------ ")
             driver.save_screenshot(os.getcwd() + "/screenshots/test_url.png")
+            assert False
+
+    def test_homePageTitle(self, setup):
+        self.logger.info("------------------ Testing Home Page Title(Login Test) ------------------ ")
+        driver = setup
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get(self.url)
+        lp = LoginPage(driver)
+        lp.setUserEmail(self.username)
+        lp.setUserPassword(self.password)
+        lp.clickLogin()
+        actTitle = driver.title
+        lp.clickLogout()
+        if actTitle == "Dashboard / nopCommerce administration":
+            self.logger.info("------------------ Home Page Title(Login Test) Passed ------------------")
+            assert True
+        else:
+            self.logger.info("------------------ Home Page Title(Login Test) Failed ------------------")
+            driver.save_screenshot(os.getcwd() + "/screenshots/test_homePageTitle.png")
             assert False
